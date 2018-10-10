907f684faf8d998a1164f8b7d895d21207f22561
cmt2 <cmt2@berkeley.edu>
2018-10-10 10:23:31 -0700

    Update runTrimGalore.sh
    adding specifics for Bomarea transcriptomes

--- runTrimGalore.sh
+++ runTrimGalore.sh
@@ -1,30 +1,28 @@
 #!/bin/bash -l
-#SBATCH --job-name=TGlane2
+#SBATCH --job-name=Bomarea_transc
 #SBATCH --partition=savio2_htc
-#SBATCH --account=co_rosalind
-#SBATCH --qos=rosalind_htc2_normal
+#SBATCH --account=fc_rothfelslab
+#SBATCH --qos=savio_normal
 #SBATCH --mail-user=username@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --time=72:00:00 
 
-module load trim_galore/0.3.7
-module load fastqc/0.11.5
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
 
-cd /clusterfs/rosalind/users/chodon/calochortus/raw
+trim_galore --fastqc -o outputdir36bp --paired CSJM003A_S33_L006_R1_001.fastq.gz CSJM003A_S33_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003B_S34_L006_R1_001.fastq.gz CSJM003B_S34_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003C_S35_L006_R1_001.fastq.gz CSJM003C_S35_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003D_S36_L006_R1_001.fastq.gz CSJM003D_S36_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003E_S37_L006_R1_001.fastq.gz CSJM003E_S37_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003F_S38_L006_R1_001.fastq.gz CSJM003F_S38_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003G_S39_L006_R1_001.fastq.gz CSJM003G_S39_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003H_S40_L006_R1_001.fastq.gz CSJM003H_S40_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003I_S41_L006_R1_001.fastq.gz CSJM003I_S41_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003J_S42_L006_R1_001.fastq.gz CSJM003J_S42_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003L_S44_L006_R1_001.fastq.gz CSJM003L_S44_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore --fastqc -o outputdir36bp --paired CSJM003k_S43_L006_R1_001.fastq.gz CSJM003k_S43_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
 
-trim_galore --fastqc -o outputdir36bp --paired CSJM002A_S1_L001_R1_001.fastq.gz CSJM002A_S1_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002B_S2_L001_R1_001.fastq.gz CSJM002B_S2_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002C_S3_L001_R1_001.fastq.gz CSJM002C_S3_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002D_S4_L001_R1_001.fastq.gz CSJM002D_S4_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002E_S5_L001_R1_001.fastq.gz CSJM002E_S5_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002F_S6_L001_R1_001.fastq.gz CSJM002F_S6_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002G_S7_L001_R1_001.fastq.gz CSJM002G_S7_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002H_S8_L001_R1_001.fastq.gz CSJM002H_S8_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002I_S9_L001_R1_001.fastq.gz CSJM002I_S9_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002J_S10_L001_R1_001.fastq.gz CSJM002J_S10_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002K_S11_L001_R1_001.fastq.gz CSJM002K_S11_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002L_S12_L001_R1_001.fastq.gz CSJM002L_S12_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore --fastqc -o outputdir36bp --paired CSJM002M_S13_L001_R1_001.fastq.gz CSJM002M_S13_L001_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+
 
 
 exit
