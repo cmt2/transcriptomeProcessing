d18549a98839080a37467fbc2a449e15b773c8a4
cmt2 <cmt2@berkeley.edu>
2018-10-13 15:32:42 -0400

    Update runTrimGalore_retry.sh
    redo files A, B, and C with increased memory (35G)

--- runTrimGalore_retry.sh
+++ runTrimGalore_retry.sh
@@ -4,14 +4,15 @@
 #SBATCH --account=fc_rothfelslab
 #SBATCH --qos=savio_normal
 #SBATCH --mail-user=cmt2@berkeley.edu
+#SBATCH --mem-per-cpu=35G
 #SBATCH --mail-type=ALL
 #SBATCH --time=72:00:00 
 
 cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
 
-trim_galore -o outputdir36bp_retry --paired CSJM003D_S36_L006_R1_001.fastq.gz CSJM003D_S36_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore -o outputdir36bp_retry --paired CSJM003F_S38_L006_R1_001.fastq.gz CSJM003F_S38_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
-trim_galore -o outputdir36bp_retry --paired CSJM003G_S39_L006_R1_001.fastq.gz CSJM003G_S39_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore -o output_final --paired CSJM003A_S36_L006_R1_001.fastq.gz CSJM003A_S36_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore -o output_final --paired CSJM003B_S38_L006_R1_001.fastq.gz CSJM003B_S38_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
+trim_galore -o output_final --paired CSJM003k_S39_L006_R1_001.fastq.gz CSJM003k_S39_L006_R2_001.fastq.gz --retain_unpaired --dont_gzip --length 36
 
 
 
