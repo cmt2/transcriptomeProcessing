b7fecc6bb60af772362f5ca66eb820a4d51d431f
cmt2 <cmt2@berkeley.edu>
2018-10-14 11:13:04 -0500

    Update moveandrename.sh
    change directories and filenames for Bomarea

--- moveandrename.sh
+++ moveandrename.sh
@@ -1,43 +1,42 @@
-mv CSJM002A_S1_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002A_2.fq
-mv CSJM002B_S2_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002B_2.fq
-mv CSJM002C_S3_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002C_2.fq
-mv CSJM002D_S4_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002D_2.fq
-mv CSJM002E_S5_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002E_2.fq
-mv CSJM002F_S6_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002F_2.fq
-mv CSJM002G_S7_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002G_2.fq
-mv CSJM002H_S8_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002H_2.fq
-mv CSJM002I_S9_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002I_2.fq
-mv CSJM002J_S10_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002J_2.fq
-mv CSJM002K_S11_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002K_2.fq
-mv CSJM002L_S12_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002L_2.fq
-mv CSJM002M_S13_L001_R2_001_val_2.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002M_2.fq
 
-mv CSJM002A_S1_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002A_1.fq
-mv CSJM002B_S2_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002B_1.fq
-mv CSJM002C_S3_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002C_1.fq
-mv CSJM002D_S4_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002D_1.fq
-mv CSJM002E_S5_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002E_1.fq
-mv CSJM002F_S6_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002F_1.fq
-mv CSJM002G_S7_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002G_1.fq
-mv CSJM002H_S8_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002H_1.fq
-mv CSJM002I_S9_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002I_1.fq
-mv CSJM002J_S10_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002J_1.fq
-mv CSJM002K_S11_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002K_1.fq
-mv CSJM002L_S12_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002L_1.fq
-mv CSJM002M_S13_L001_R1_001_val_1.fq /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002M_1.fq
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
+mkdir clean
 
-cat CSJM002A_S1_L001_R1_001_unpaired_1.fq CSJM002A_S1_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002A_u.fq
-cat CSJM002B_S2_L001_R1_001_unpaired_1.fq CSJM002B_S2_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002B_u.fq
-cat CSJM002C_S3_L001_R1_001_unpaired_1.fq CSJM002C_S3_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002C_u.fq
-cat CSJM002D_S4_L001_R1_001_unpaired_1.fq CSJM002D_S4_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002D_u.fq
-cat CSJM002E_S5_L001_R1_001_unpaired_1.fq CSJM002E_S5_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002E_u.fq
-cat CSJM002F_S6_L001_R1_001_unpaired_1.fq CSJM002F_S6_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002F_u.fq
-cat CSJM002G_S7_L001_R1_001_unpaired_1.fq CSJM002G_S7_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002G_u.fq
-cat CSJM002H_S8_L001_R1_001_unpaired_1.fq CSJM002H_S8_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002H_u.fq
-cat CSJM002I_S9_L001_R1_001_unpaired_1.fq CSJM002I_S9_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002I_u.fq
-cat CSJM002J_S10_L001_R1_001_unpaired_1.fq CSJM002J_S10_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002J_u.fq
-cat CSJM002K_S11_L001_R1_001_unpaired_1.fq CSJM002K_S11_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002K_u.fq
-cat CSJM002L_S12_L001_R1_001_unpaired_1.fq CSJM002L_S12_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002L_u.fq
-cat CSJM002M_S13_L001_R1_001_unpaired_1.fq CSJM002M_S13_L001_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > /clusterfs/rosalind/users/chodon/calochortus/clean/CSJM002M_u.fq
+mv output_final/CSJM003G_S39_L006_R2_001_val_2.fq clean/CSJM003G_2.fq 
+mv output_final/CSJM003E_S37_L006_R2_001_val_2.fq clean/CSJM003E_2.fq 
+mv output_final/CSJM003J_S42_L006_R2_001_val_2.fq clean/CSJM003J_2.fq 
+mv output_final/CSJM003I_S41_L006_R2_001_val_2.fq clean/CSJM003I_2.fq 
+mv output_final/CSJM003k_S43_L006_R2_001_val_2.fq clean/CSJM003k_2.fq 
+mv output_final/CSJM003B_S34_L006_R2_001_val_2.fq clean/CSJM003B_2.fq 
+mv output_final/CSJM003D_S36_L006_R2_001_val_2.fq clean/CSJM003D_2.fq 
+mv output_final/CSJM003L_S44_L006_R2_001_val_2.fq clean/CSJM003L_2.fq 
+mv output_final/CSJM003A_S33_L006_R2_001_val_2.fq clean/CSJM003A_2.fq 
+mv output_final/CSJM003C_S35_L006_R2_001_val_2.fq clean/CSJM003C_2.fq 
+mv output_final/CSJM003H_S40_L006_R2_001_val_2.fq clean/CSJM003H_2.fq 
+mv output_final/CSJM003F_S38_L006_R2_001_val_2.fq clean/CSJM003F_2.fq 
 
+mv output_final/CSJM003F_S38_L006_R1_001_val_1.fq clean/CSJM003F_1.fq
+mv output_final/CSJM003C_S35_L006_R1_001_val_1.fq clean/CSJM003C_1.fq
+mv output_final/CSJM003G_S39_L006_R1_001_val_1.fq clean/CSJM003G_1.fq
+mv output_final/CSJM003B_S34_L006_R1_001_val_1.fq clean/CSJM003B_1.fq
+mv output_final/CSJM003J_S42_L006_R1_001_val_1.fq clean/CSJM003J_1.fq
+mv output_final/CSJM003L_S44_L006_R1_001_val_1.fq clean/CSJM003L_1.fq
+mv output_final/CSJM003I_S41_L006_R1_001_val_1.fq clean/CSJM003I_1.fq
+mv output_final/CSJM003H_S40_L006_R1_001_val_1.fq clean/CSJM003H_1.fq
+mv output_final/CSJM003k_S43_L006_R1_001_val_1.fq clean/CSJM003k_1.fq
+mv output_final/CSJM003E_S37_L006_R1_001_val_1.fq clean/CSJM003E_1.fq
+mv output_final/CSJM003A_S33_L006_R1_001_val_1.fq clean/CSJM003A_1.fq
+mv output_final/CSJM003D_S36_L006_R1_001_val_1.fq clean/CSJM003D_1.fq
 
+cat output_final/CSJM003E_S37_L006_R1_001_unpaired_1.fq output_final/CSJM003E_S37_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003E_u.fq
+cat output_final/CSJM003L_S44_L006_R1_001_unpaired_1.fq output_final/CSJM003L_S44_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003L_u.fq
+cat output_final/CSJM003G_S39_L006_R1_001_unpaired_1.fq output_final/CSJM003G_S39_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003G_u.fq
+cat output_final/CSJM003I_S41_L006_R1_001_unpaired_1.fq output_final/CSJM003I_S41_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003I_u.fq
+cat output_final/CSJM003J_S42_L006_R1_001_unpaired_1.fq output_final/CSJM003J_S42_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003J_u.fq
+cat output_final/CSJM003A_S33_L006_R1_001_unpaired_1.fq output_final/CSJM003A_S33_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003A_u.fq
+cat output_final/CSJM003B_S34_L006_R1_001_unpaired_1.fq output_final/CSJM003B_S34_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003B_u.fq
+cat output_final/CSJM003D_S36_L006_R1_001_unpaired_1.fq output_final/CSJM003D_S36_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003D_u.fq
+cat output_final/CSJM003F_S38_L006_R1_001_unpaired_1.fq output_final/CSJM003F_S38_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003F_u.fq
+cat output_final/CSJM003k_S43_L006_R1_001_unpaired_1.fq output_final/CSJM003k_S43_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003k_u.fq
+cat output_final/CSJM003C_S35_L006_R1_001_unpaired_1.fq output_final/CSJM003C_S35_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003C_u.fq
+cat output_final/CSJM003H_S40_L006_R1_001_unpaired_1.fq output_final/CSJM003H_S40_L006_R1_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003H_u.fq
