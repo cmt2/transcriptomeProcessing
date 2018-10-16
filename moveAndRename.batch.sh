3d27fceaf0a7e1dd84f8089fe81bfe5db0e6e44a
cmt2 <cmt2@berkeley.edu>
2018-10-16 16:59:55 -0500

    Update moveAndRename.batch.sh
    remove mem per CPU line

--- moveAndRename.batch.sh
+++ moveAndRename.batch.sh
@@ -5,7 +5,6 @@
 #SBATCH --qos=savio_normal
 #SBATCH --mail-user=cmt2@berkeley.edu
 #SBATCH --mail-type=ALL
-#SBATCH --mem-per-cpu=35G
 #SBATCH --time=72:00:00 
 
 bash transcriptomeProcessing/moveandrename.sh
