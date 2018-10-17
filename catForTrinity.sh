28a8b7a7a504b71fe511549e498e50564bc96dbd
cmt2 <cmt2@berkeley.edu>
2018-10-16 22:53:40 -0500

    Update catForTrinity.sh
    fix path to output files

--- catForTrinity.sh
+++ catForTrinity.sh
@@ -1,6 +1,6 @@
 cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
 mkdir bom_mult
 
-cat clean/CSJM003H_1.fq clean/CSJM003F_1.fq clean/CSJM003G_1.fq clean/CSJM003J_1.fq clean/CSJM003D_1.fq clean/CSJM003k_1.fq clean/CSJM003I_1.fq clean/CSJM003B_1.fq clean/CSJM003L_1.fq clean/CSJM003C_1.fq clean/CSJM003A_1.fq clean/CSJM003E_1.fq > ../bom_mult/bom_mult_1.fq
-cat clean/CSJM003B_2.fq clean/CSJM003J_2.fq clean/CSJM003E_2.fq clean/CSJM003I_2.fq clean/CSJM003H_2.fq clean/CSJM003C_2.fq clean/CSJM003D_2.fq clean/CSJM003A_2.fq clean/CSJM003L_2.fq clean/CSJM003k_2.fq clean/CSJM003F_2.fq clean/CSJM003G_2.fq > ../bom_mult/bom_mult_2.fq
+cat clean/CSJM003H_1.fq clean/CSJM003F_1.fq clean/CSJM003G_1.fq clean/CSJM003J_1.fq clean/CSJM003D_1.fq clean/CSJM003k_1.fq clean/CSJM003I_1.fq clean/CSJM003B_1.fq clean/CSJM003L_1.fq clean/CSJM003C_1.fq clean/CSJM003A_1.fq clean/CSJM003E_1.fq > bom_mult/bom_mult_1.fq
+cat clean/CSJM003B_2.fq clean/CSJM003J_2.fq clean/CSJM003E_2.fq clean/CSJM003I_2.fq clean/CSJM003H_2.fq clean/CSJM003C_2.fq clean/CSJM003D_2.fq clean/CSJM003A_2.fq clean/CSJM003L_2.fq clean/CSJM003k_2.fq clean/CSJM003F_2.fq clean/CSJM003G_2.fq > bom_mult/bom_mult_2.fq
 
