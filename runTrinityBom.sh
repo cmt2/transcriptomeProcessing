60787387dddb6134bb92f5f5d75fada5ce2da9b4
cmt2 <cmt2@berkeley.edu>
2018-10-17 09:05:08 -0500

    Update and rename runTrinityCven.sh to runTrinityBom.sh
    change Trinity script for Bomarea

---
+++ runTrinityBom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=TBom
+#SBATCH --partition=savio_bigmem
+#SBATCH --account=fc_rothfelslab
+#SBATCH --qos=savio_normal
+#SBATCH --mail-user=cmt2@berkeley.edu
+#SBATCH --mem-per-cpu=25G
+#SBATCH --cpus-per-task=20
+#SBATCH --mail-type=ALL
+#SBATCH --time=72:00:00 
+
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea/bom_mult
+
+Trinity --SS_lib_type RF --seqType fq --max_memory 400G --left bom_mult_1.fq --right bom_mult_2.fq --CPU 20 --output trinity
+
+exit
