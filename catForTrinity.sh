d7b0fabda8d174375dc063bd0fb3b341dd94d8d8
cmt2 <cmt2@berkeley.edu>
2018-10-16 22:42:57 -0500

    Update catForTrinity.sh
    Changes filenames to match Bomarea files

--- catForTrinity.sh
+++ catForTrinity.sh
@@ -1,17 +1,6 @@
-bzcat CSJM001A_1.fq.bz2 CSJM001B_1.fq.bz2 CSJM001C_1.fq.bz2 CSJM001D_1.fq.bz2 CSJM001E_1.fq.bz2 CSJM001F_1.fq.bz2 CSJM001N_1.fq.bz2  > ../cmon/cmon_1.fq
-bzcat CSJM001A_2.fq.bz2 CSJM001B_2.fq.bz2 CSJM001C_2.fq.bz2 CSJM001D_2.fq.bz2 CSJM001E_2.fq.bz2 CSJM001F_2.fq.bz2 CSJM001N_2.fq.bz2  > ../cmon/cmon_2.fq
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
+mkdir bom_mult
 
-bzcat CSJM001G_1.fq.bz2 CSJM001H_1.fq.bz2 CSJM001I_1.fq.bz2 CSJM001J_1.fq.bz2 CSJM001K_1.fq.bz2 CSJM001L_1.fq.bz2 > ../cama/cama_1.fq
-bzcat CSJM001G_2.fq.bz2 CSJM001H_2.fq.bz2 CSJM001I_2.fq.bz2 CSJM001J_2.fq.bz2 CSJM001K_2.fq.bz2 CSJM001L_2.fq.bz2 > ../cama/cama_2.fq
-
-
-cp CSJM002E_1.fq ../cumb/cumb_1.fq
-cp CSJM002E_2.fq ../cumb/cumb_2.fq
-
-
-bzcat CSJM001M_1.fq.bz2 > ../calb/calb_1.fq
-bzcat CSJM001M_2.fq.bz2 > ../calb/calb_2.fq
-
-cat CSJM002A_1.fq CSJM002B_1.fq CSJM002C_1.fq CSJM002D_1.fq CSJM002F_1.fq CSJM002G_1.fq CSJM002H_1.fq CSJM002I_1.fq CSJM002J_1.fq CSJM002K_1.fq CSJM002L_1.fq CSJM002M_1.fq > ../cven/cven_1.fq
-cat CSJM002A_2.fq CSJM002B_2.fq CSJM002C_2.fq CSJM002D_2.fq CSJM002F_2.fq CSJM002G_2.fq CSJM002H_2.fq CSJM002I_2.fq CSJM002J_2.fq CSJM002K_2.fq CSJM002L_2.fq CSJM002M_2.fq > ../cven/cven_2.fq
+cat clean/CSJM003H_1.fq clean/CSJM003F_1.fq clean/CSJM003G_1.fq clean/CSJM003J_1.fq clean/CSJM003D_1.fq clean/CSJM003k_1.fq clean/CSJM003I_1.fq clean/CSJM003B_1.fq clean/CSJM003L_1.fq clean/CSJM003C_1.fq clean/CSJM003A_1.fq clean/CSJM003E_1.fq > ../bom_mult/bom_mult_1.fq
+cat clean/CSJM003B_2.fq clean/CSJM003J_2.fq clean/CSJM003E_2.fq clean/CSJM003I_2.fq clean/CSJM003H_2.fq clean/CSJM003C_2.fq clean/CSJM003D_2.fq clean/CSJM003A_2.fq clean/CSJM003L_2.fq clean/CSJM003k_2.fq clean/CSJM003F_2.fq clean/CSJM003G_2.fq > ../bom_mult/bom_mult_2.fq
 
