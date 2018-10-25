73740c83694075681c583b0d522268893e7179df
cmt2 <cmt2@berkeley.edu>
2018-10-25 13:19:28 -0700

    Update runTrinityBom.sh
    archive original trinity run folder in a copy before restarting run

--- runTrinityBom.sh
+++ runTrinityBom.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=72:00:00 
 
 cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea/bom_mult
-rm -R trinity
+cp trinity trinity_orig
 
 Trinity --SS_lib_type RF --seqType fq --max_memory 400G --left bom_mult_1.fq --right bom_mult_2.fq --CPU 20 --output trinity
 
