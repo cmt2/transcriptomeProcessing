445f92513c9d257a7fcccaf0925644078dfb0fc5
cmt2 <cmt2@berkeley.edu>
2018-10-31 07:39:10 -0700

    Update moveandrename.sh
    update for Jesus' transcriptomes

--- moveandrename.sh
+++ moveandrename.sh
@@ -1,43 +1,83 @@
 
-cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Jesus
+
+mkdir output_final
+mkdir clean
 
 cp outputdir36bp/* output_final/
 
-mv output_final/CSJM003G_S39_L006_R2_001_val_2.fq clean/CSJM003G_2.fq 
-mv output_final/CSJM003E_S37_L006_R2_001_val_2.fq clean/CSJM003E_2.fq 
-mv output_final/CSJM003J_S42_L006_R2_001_val_2.fq clean/CSJM003J_2.fq 
-mv output_final/CSJM003I_S41_L006_R2_001_val_2.fq clean/CSJM003I_2.fq 
-mv output_final/CSJM003k_S43_L006_R2_001_val_2.fq clean/CSJM003k_2.fq 
-mv output_final/CSJM003B_S34_L006_R2_001_val_2.fq clean/CSJM003B_2.fq 
-mv output_final/CSJM003D_S36_L006_R2_001_val_2.fq clean/CSJM003D_2.fq 
-mv output_final/CSJM003L_S44_L006_R2_001_val_2.fq clean/CSJM003L_2.fq 
-mv output_final/CSJM003A_S33_L006_R2_001_val_2.fq clean/CSJM003A_2.fq 
-mv output_final/CSJM003C_S35_L006_R2_001_val_2.fq clean/CSJM003C_2.fq 
-mv output_final/CSJM003H_S40_L006_R2_001_val_2.fq clean/CSJM003H_2.fq 
-mv output_final/CSJM003F_S38_L006_R2_001_val_2.fq clean/CSJM003F_2.fq 
 
-mv output_final/CSJM003F_S38_L006_R1_001_val_1.fq clean/CSJM003F_1.fq
-mv output_final/CSJM003C_S35_L006_R1_001_val_1.fq clean/CSJM003C_1.fq
-mv output_final/CSJM003G_S39_L006_R1_001_val_1.fq clean/CSJM003G_1.fq
-mv output_final/CSJM003B_S34_L006_R1_001_val_1.fq clean/CSJM003B_1.fq
-mv output_final/CSJM003J_S42_L006_R1_001_val_1.fq clean/CSJM003J_1.fq
-mv output_final/CSJM003L_S44_L006_R1_001_val_1.fq clean/CSJM003L_1.fq
-mv output_final/CSJM003I_S41_L006_R1_001_val_1.fq clean/CSJM003I_1.fq
-mv output_final/CSJM003H_S40_L006_R1_001_val_1.fq clean/CSJM003H_1.fq
-mv output_final/CSJM003k_S43_L006_R1_001_val_1.fq clean/CSJM003k_1.fq
-mv output_final/CSJM003E_S37_L006_R1_001_val_1.fq clean/CSJM003E_1.fq
-mv output_final/CSJM003A_S33_L006_R1_001_val_1.fq clean/CSJM003A_1.fq
-mv output_final/CSJM003D_S36_L006_R1_001_val_1.fq clean/CSJM003D_1.fq
+mv output_final/CSJM005F_S62_L008_R2_001_val_2.fq clean/CSJM005F_2.fq
+mv output_final/CSJM005D_S60_L008_R2_001_val_2.fq clean/CSJM005D_2.fq
+mv output_final/CSJM004k_S55_L007_R2_001_val_2.fq clean/CSJM004k_2.fq
+mv output_final/CSJM005J_S66_L008_R2_001_val_2.fq clean/CSJM005J_2.fq
+mv output_final/CSJM004A_S45_L007_R2_001_val_2.fq clean/CSJM004A_2.fq
+mv output_final/CSJM004L_S56_L007_R2_001_val_2.fq clean/CSJM004L_2.fq
+mv output_final/CSJM005G_S63_L008_R2_001_val_2.fq clean/CSJM005G_2.fq
+mv output_final/CSJM005C_S59_L008_R2_001_val_2.fq clean/CSJM005C_2.fq
+mv output_final/CSJM004C_S47_L007_R2_001_val_2.fq clean/CSJM004C_2.fq
+mv output_final/CSJM004D_S48_L007_R2_001_val_2.fq clean/CSJM004D_2.fq
+mv output_final/CSJM005B_S58_L008_R2_001_val_2.fq clean/CSJM005B_2.fq
+mv output_final/CSJM005L_S68_L008_R2_001_val_2.fq clean/CSJM005L_2.fq
+mv output_final/CSJM004G_S51_L007_R2_001_val_2.fq clean/CSJM004G_2.fq
+mv output_final/CSJM005E_S61_L008_R2_001_val_2.fq clean/CSJM005E_2.fq
+mv output_final/CSJM004I_S53_L007_R2_001_val_2.fq clean/CSJM004I_2.fq
+mv output_final/CSJM004F_S50_L007_R2_001_val_2.fq clean/CSJM004F_2.fq
+mv output_final/CSJM004J_S54_L007_R2_001_val_2.fq clean/CSJM004J_2.fq
+mv output_final/CSJM004E_S49_L007_R2_001_val_2.fq clean/CSJM004E_2.fq
+mv output_final/CSJM004B_S46_L007_R2_001_val_2.fq clean/CSJM004B_2.fq
+mv output_final/CSJM005H_S64_L008_R2_001_val_2.fq clean/CSJM005H_2.fq
+mv output_final/CSJM004H_S52_L007_R2_001_val_2.fq clean/CSJM004H_2.fq
+mv output_final/CSJM005k_S67_L008_R2_001_val_2.fq clean/CSJM005k_2.fq
+mv output_final/CSJM005I_S65_L008_R2_001_val_2.fq clean/CSJM005I_2.fq
+mv output_final/CSJM005A_S57_L008_R2_001_val_2.fq clean/CSJM005A_2.fq
 
-cat output_final/CSJM003E_S37_L006_R1_001_unpaired_1.fq output_final/CSJM003E_S37_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003E_u.fq
-cat output_final/CSJM003L_S44_L006_R1_001_unpaired_1.fq output_final/CSJM003L_S44_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003L_u.fq
-cat output_final/CSJM003G_S39_L006_R1_001_unpaired_1.fq output_final/CSJM003G_S39_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003G_u.fq
-cat output_final/CSJM003I_S41_L006_R1_001_unpaired_1.fq output_final/CSJM003I_S41_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003I_u.fq
-cat output_final/CSJM003J_S42_L006_R1_001_unpaired_1.fq output_final/CSJM003J_S42_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003J_u.fq
-cat output_final/CSJM003A_S33_L006_R1_001_unpaired_1.fq output_final/CSJM003A_S33_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003A_u.fq
-cat output_final/CSJM003B_S34_L006_R1_001_unpaired_1.fq output_final/CSJM003B_S34_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003B_u.fq
-cat output_final/CSJM003D_S36_L006_R1_001_unpaired_1.fq output_final/CSJM003D_S36_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003D_u.fq
-cat output_final/CSJM003F_S38_L006_R1_001_unpaired_1.fq output_final/CSJM003F_S38_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003F_u.fq
-cat output_final/CSJM003k_S43_L006_R1_001_unpaired_1.fq output_final/CSJM003k_S43_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003k_u.fq
-cat output_final/CSJM003C_S35_L006_R1_001_unpaired_1.fq output_final/CSJM003C_S35_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003C_u.fq
-cat output_final/CSJM003H_S40_L006_R1_001_unpaired_1.fq output_final/CSJM003H_S40_L006_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM003H_u.fq
+mv output_final/CSJM004B_S46_L007_R1_001_val_1.fq clean/CSJM004B_1.fq
+mv output_final/CSJM005H_S64_L008_R1_001_val_1.fq clean/CSJM005H_1.fq
+mv output_final/CSJM004E_S49_L007_R1_001_val_1.fq clean/CSJM004E_1.fq
+mv output_final/CSJM005C_S59_L008_R1_001_val_1.fq clean/CSJM005C_1.fq
+mv output_final/CSJM005J_S66_L008_R1_001_val_1.fq clean/CSJM005J_1.fq
+mv output_final/CSJM005E_S61_L008_R1_001_val_1.fq clean/CSJM005E_1.fq
+mv output_final/CSJM004F_S50_L007_R1_001_val_1.fq clean/CSJM004F_1.fq
+mv output_final/CSJM005D_S60_L008_R1_001_val_1.fq clean/CSJM005D_1.fq
+mv output_final/CSJM004H_S52_L007_R1_001_val_1.fq clean/CSJM004H_1.fq
+mv output_final/CSJM005F_S62_L008_R1_001_val_1.fq clean/CSJM005F_1.fq
+mv output_final/CSJM004D_S48_L007_R1_001_val_1.fq clean/CSJM004D_1.fq
+mv output_final/CSJM005B_S58_L008_R1_001_val_1.fq clean/CSJM005B_1.fq
+mv output_final/CSJM005G_S63_L008_R1_001_val_1.fq clean/CSJM005G_1.fq
+mv output_final/CSJM004k_S55_L007_R1_001_val_1.fq clean/CSJM004k_1.fq
+mv output_final/CSJM004I_S53_L007_R1_001_val_1.fq clean/CSJM004I_1.fq
+mv output_final/CSJM004L_S56_L007_R1_001_val_1.fq clean/CSJM004L_1.fq
+mv output_final/CSJM004J_S54_L007_R1_001_val_1.fq clean/CSJM004J_1.fq
+mv output_final/CSJM004A_S45_L007_R1_001_val_1.fq clean/CSJM004A_1.fq
+mv output_final/CSJM005k_S67_L008_R1_001_val_1.fq clean/CSJM005k_1.fq
+mv output_final/CSJM004G_S51_L007_R1_001_val_1.fq clean/CSJM004G_1.fq
+mv output_final/CSJM005I_S65_L008_R1_001_val_1.fq clean/CSJM005I_1.fq
+mv output_final/CSJM005L_S68_L008_R1_001_val_1.fq clean/CSJM005L_1.fq
+mv output_final/CSJM004C_S47_L007_R1_001_val_1.fq clean/CSJM004C_1.fq
+mv output_final/CSJM005A_S57_L008_R1_001_val_1.fq clean/CSJM005A_1.fq
+
+cat output_final/CSJM005H_S64_L008_R1_001_unpaired_1.fq output_final/CSJM005H_S64_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005H_u.fq
+cat output_final/CSJM005L_S68_L008_R1_001_unpaired_1.fq output_final/CSJM005L_S68_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005L_u.fq
+cat output_final/CSJM005A_S57_L008_R1_001_unpaired_1.fq output_final/CSJM005A_S57_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005A_u.fq
+cat output_final/CSJM005I_S65_L008_R1_001_unpaired_1.fq output_final/CSJM005I_S65_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005I_u.fq
+cat output_final/CSJM004A_S45_L007_R1_001_unpaired_1.fq output_final/CSJM004A_S45_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004A_u.fq
+cat output_final/CSJM005G_S63_L008_R1_001_unpaired_1.fq output_final/CSJM005G_S63_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005G_u.fq
+cat output_final/CSJM004G_S51_L007_R1_001_unpaired_1.fq output_final/CSJM004G_S51_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004G_u.fq
+cat output_final/CSJM005B_S58_L008_R1_001_unpaired_1.fq output_final/CSJM005B_S58_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005B_u.fq
+cat output_final/CSJM004F_S50_L007_R1_001_unpaired_1.fq output_final/CSJM004F_S50_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004F_u.fq
+cat output_final/CSJM004D_S48_L007_R1_001_unpaired_1.fq output_final/CSJM004D_S48_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004D_u.fq
+cat output_final/CSJM004B_S46_L007_R1_001_unpaired_1.fq output_final/CSJM004B_S46_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004B_u.fq
+cat output_final/CSJM004H_S52_L007_R1_001_unpaired_1.fq output_final/CSJM004H_S52_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004H_u.fq
+cat output_final/CSJM005F_S62_L008_R1_001_unpaired_1.fq output_final/CSJM005F_S62_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005F_u.fq
+cat output_final/CSJM005C_S59_L008_R1_001_unpaired_1.fq output_final/CSJM005C_S59_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005C_u.fq
+cat output_final/CSJM005k_S67_L008_R1_001_unpaired_1.fq output_final/CSJM005k_S67_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005k_u.fq
+cat output_final/CSJM005D_S60_L008_R1_001_unpaired_1.fq output_final/CSJM005D_S60_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005D_u.fq
+cat output_final/CSJM004L_S56_L007_R1_001_unpaired_1.fq output_final/CSJM004L_S56_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004L_u.fq
+cat output_final/CSJM005E_S61_L008_R1_001_unpaired_1.fq output_final/CSJM005E_S61_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005E_u.fq
+cat output_final/CSJM004E_S49_L007_R1_001_unpaired_1.fq output_final/CSJM004E_S49_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004E_u.fq
+cat output_final/CSJM004k_S55_L007_R1_001_unpaired_1.fq output_final/CSJM004k_S55_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004k_u.fq
+cat output_final/CSJM005J_S66_L008_R1_001_unpaired_1.fq output_final/CSJM005J_S66_L008_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM005J_u.fq
+cat output_final/CSJM004I_S53_L007_R1_001_unpaired_1.fq output_final/CSJM004I_S53_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004I_u.fq
+cat output_final/CSJM004J_S54_L007_R1_001_unpaired_1.fq output_final/CSJM004J_S54_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004J_u.fq
+cat output_final/CSJM004C_S47_L007_R1_001_unpaired_1.fq output_final/CSJM004C_S47_L007_R2_001_unpaired_2.fq | sed 's/ 2:/ 1:/g' > clean/CSJM004C_u.fq
