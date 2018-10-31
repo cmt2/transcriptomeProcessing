e61c84834aa3f5e2790daef860356b7101505631
cmt2 <cmt2@berkeley.edu>
2018-10-31 15:30:38 -0700

    Update catForTrinity.sh
    update files for Jesus' transcriptomes

--- catForTrinity.sh
+++ catForTrinity.sh
@@ -1,6 +1,9 @@
-cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Bomarea
-mkdir bom_mult
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Jesus
+mkdir allium
+mkdir hemero
 
-cat clean/CSJM003A_1.fq clean/CSJM003B_1.fq clean/CSJM003C_1.fq clean/CSJM003D_1.fq clean/CSJM003E_1.fq clean/CSJM003F_1.fq clean/CSJM003G_1.fq clean/CSJM003H_1.fq clean/CSJM003I_1.fq clean/CSJM003J_1.fq clean/CSJM003k_1.fq clean/CSJM003L_1.fq > bom_mult/bom_mult_1.fq
-cat clean/CSJM003A_2.fq clean/CSJM003B_2.fq clean/CSJM003C_2.fq clean/CSJM003D_2.fq clean/CSJM003E_2.fq clean/CSJM003F_2.fq clean/CSJM003G_2.fq clean/CSJM003H_2.fq clean/CSJM003I_2.fq clean/CSJM003J_2.fq clean/CSJM003k_2.fq clean/CSJM003L_2.fq > bom_mult/bom_mult_2.fq
+cat clean/CSJM004C_1.fq clean/CSJM004D_1.fq clean/CSJM004L_1.fq clean/CSJM004E_1.fq clean/CSJM004F_1.fq clean/CSJM004G_1.fq clean/CSJM004k_1.fq clean/CSJM004H_1.fq clean/CSJM004J_1.fq clean/CSJM004A_1.fq clean/CSJM004B_1.fq clean/CSJM004I_1.fq clean/CSJM005G_1.fq clean/CSJM005H_1.fq clean/CSJM005I_1.fq clean/CSJM005J_1.fq clean/CSJM005k_1.fq clean/CSJM005L_1.fq > allium/allium_1.fq
+cat clean/CSJM004C_2.fq clean/CSJM004D_2.fq clean/CSJM004L_2.fq clean/CSJM004E_2.fq clean/CSJM004F_2.fq clean/CSJM004G_2.fq clean/CSJM004k_2.fq clean/CSJM004H_2.fq clean/CSJM004J_2.fq clean/CSJM004A_2.fq clean/CSJM004B_2.fq clean/CSJM004I_2.fq clean/CSJM005G_2.fq clean/CSJM005H_2.fq clean/CSJM005I_2.fq clean/CSJM005J_2.fq clean/CSJM005k_2.fq clean/CSJM005L_2.fq > allium/allium_2.fq
 
+cat clean/CSJM005A_1.fq clean/CSJM005B_1.fq clean/CSJM005C_1.fq clean/CSJM005D_1.fq clean/CSJM005E_1.fq clean/CSJM005F_1.fq > hemero/hemero_1.fq
+cat clean/CSJM005A_2.fq clean/CSJM005B_2.fq clean/CSJM005C_2.fq clean/CSJM005D_2.fq clean/CSJM005E_2.fq clean/CSJM005F_2.fq > hemero/hemero_2.fq
