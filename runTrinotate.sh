c167196cbb7e607fde7a2ec97ed6e697db5bd63b
cmt2 <cmt2@berkeley.edu>
2018-11-06 22:15:56 -0800

    Update runTrinotate.sh
    starting changes to change for fc_rothfels account and program set up

--- runTrinotate.sh
+++ runTrinotate.sh
@@ -1,30 +1,26 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=3NCven
+#SBATCH --job-name=3NAllium
 #SBATCH --partition=savio
-#SBATCH --account=co_rosalind
-#SBATCH --qos=rosalind_savio_normal
-#SBATCH --mail-user=username@berkeley.edu
+#SBATCH --account=fc_rothfels
+#SBATCH --qos=savio_normal
+#SBATCH --mail-user=cmt2@berkeley.edu
 #SBATCH --mem-per-cpu=5G
 #SBATCH --cpus-per-task=10
 #SBATCH --mail-type=ALL
 #SBATCH --time=72:00:00 
 
-PERL5LIB=""
-#source activate trinityEnv
-module load trinity/2.4.0
+cd /global/scratch/cmt2/transcriptome_raw_data/transcriptomes_Jesus/allium/trinity
 
-cd /clusterfs/rosalind/users/chodon/calochortus/cven/trinity/
+/global/home/users/cmt2/trinityrnaseq-Trinity-v2.8.4/util/support_scripts/get_Trinity_gene_to_trans_map.pl Trinity.fasta > Trinity.fasta.gene_trans_map
 
-/clusterfs/vector/home/groups/software/sl-6.x86_64/modules/trinityrnaseq-Trinity-v2.4.0/util/support_scripts/get_Trinity_gene_to_trans_map.pl Trinity.fasta > Trinity.fasta.gene_trans_map
 
-#source deactivate
-export PERL5LIB=/global/home/users/chodon/bin/Trinotate-3.0.2/PerlLib/:/global/home/users/chodon/perl5/lib/perl5/
 module load blast
-module load hmmer/3.1b-intel
+module load hmmer # this is a different version than what Chodon used (3.1b-intel)
 
-cp /clusterfs/rosalind/users/chodon/databases/Trinotate.sqlite . 
+cp /clusterfs/rosalind/users/chodon/databases/Trinotate.sqlite . #where is this? 
 
+#edit lines below for fc_rothfels set up
 /clusterfs/rosalind/users/chodon/calochortus/auto/autoTrinotate.pl  --Trinotate_sqlite /clusterfs/rosalind/users/chodon/calochortus/cama/trinity/Trinotate.sqlite --transcripts Trinity.fasta --gene_to_trans_map Trinity.fasta.gene_trans_map --conf /clusterfs/rosalind/users/chodon/calochortus/auto/conf.txt --CPU 10
 
 /global/home/users/chodon/bin/Trinotate-3.0.2/Trinotate Trinotate.sqlite init --gene_trans_map Trinity.fasta.gene_trans_map --transcript_fasta Trinity.fasta --transdecoder_pep Trinity.fasta.transdecoder.pep
